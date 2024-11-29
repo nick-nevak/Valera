290d1f7391714418c8d0f58c883bf2874922df01
Nick <57493390+nick-nevak@users.noreply.github.com>
2024-11-30 00:31:24 +0300

    before moduling DBSCAN + PCA

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import numpy as np
 import seaborn as sns
 
 
@@ -8,13 +9,32 @@ def visualize_target_distribution(data, target_column):
     # Plot Distribution (Histogram + KDE)
     plt.figure(figsize=(10, 6))
     sns.histplot(y, kde=True, bins=50)
-    plt.title(f'Distribution of {target_column}')
+    plt.title(f"Distribution of {target_column}")
     plt.xlabel(target_column)
     plt.show()
 
     # Plot Box Plot
     plt.figure(figsize=(10, 6))
     sns.boxplot(x=y)
-    plt.title(f'Box Plot of {target_column}')
+    plt.title(f"Box Plot of {target_column}")
     plt.xlabel(target_column)
+    plt.show()
+
+
+# Visualize PCA Explained Variance
+def visualize_variance_PCA(pca, target_variance=None):
+    cumulative_variance = np.cumsum(pca.explained_variance_ratio_)
+    # Find the number of components needed for 90% variance
+    if target_variance:
+        n_components = np.argmax(cumulative_variance >= target_variance) + 1
+        print(
+            f"Number of components to retain {target_variance * 100}% variance: {n_components}"
+        )
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(cumulative_variance)
+    plt.title("Cumulative Explained Variance by PCA Components")
+    plt.xlabel("Number of Components")
+    plt.ylabel("Cumulative Explained Variance")
+    plt.grid()
     plt.show()
